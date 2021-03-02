7aa253f6fffb0449ad90751d698e931d9e750ede
Christopher <calb200@aucklanduni.ac.nz>
2021-03-03 10:03:55 +1300

    setup css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,30 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: './client/index.js',
+  entry: "./client/index.js",
   output: {
-    path: path.join(__dirname, '/public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js",
   },
-  mode: 'development',
+  mode: "development",
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
-    contentBase: './public'
-  }
-}
+    contentBase: "./public",
+  },
+};

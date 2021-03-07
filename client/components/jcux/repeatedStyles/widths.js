4ed70040a304f76a9f35e282216d14de974e9d74
Christopher <calb200@aucklanduni.ac.nz>
2021-03-08 09:51:19 +1300

    rearrange order of breakpoints in widths.js

--- client/components/jcux/repeatedStyles/widths.js
+++ client/components/jcux/repeatedStyles/widths.js
@@ -4,42 +4,42 @@ const widths = (props) => {
   if (props.width) {
     if (typeof props.width === "string") widths += `width: ${props.width};`;
     else {
-      if (props.width.xs)
-        widths += `@media (${breakpoints.xs}){
-          width: ${props.width.xs}
+      if (props.width.lg)
+        widths += `@media (${breakpoints.lg}){
+        width: ${props.width.lg}
          }`;
+      if (props.width.md)
+        widths += `@media (${breakpoints.md}){
+         width: ${props.width.md}
+          }`;
       if (props.width.sm)
         widths += `@media (${breakpoints.sm}){
         width: ${props.width.sm}
          }`;
-      if (props.width.md)
-        widths += `@media (${breakpoints.md}){
-        width: ${props.width.md}
-         }`;
-      if (props.width.lg)
-        widths += `@media (${breakpoints.lg}){
-        width: ${props.width.lg}
+      if (props.width.xs)
+        widths += `@media (${breakpoints.xs}){
+          width: ${props.width.xs}
          }`;
     }
     if (props.maxWidth) {
       if (typeof props.maxWidth === "string")
         widths += `width: ${props.maxWidth};`;
       else {
-        if (props.maxWidth.xs)
-          widths += `@media (${breakpoints.xs}){
-            width: ${props.maxWidth.xs}
-           }`;
-        if (props.maxWidth.sm)
-          widths += `@media (${breakpoints.sm}){
-          width: ${props.maxWidth.sm}
-           }`;
-        if (props.maxWidth.md)
-          widths += `@media (${breakpoints.md}){
-          width: ${props.maxWidth.md}
-           }`;
         if (props.maxWidth.lg)
           widths += `@media (${breakpoints.lg}){
           width: ${props.maxWidth.lg}
+           }`;
+        if (props.maxWidth.md)
+          widths += `@media (${breakpoints.md}){
+           width: ${props.maxWidth.md}
+            }`;
+        if (props.maxWidth.sm)
+          widths += `@media (${breakpoints.sm}){
+            width: ${props.maxWidth.sm}
+             }`;
+        if (props.maxWidth.xs)
+          widths += `@media (${breakpoints.xs}){
+            width: ${props.maxWidth.xs}
            }`;
       }
     }
@@ -47,21 +47,21 @@ const widths = (props) => {
       if (typeof props.minWidth === "string")
         widths += `width: ${props.minWidth};`;
       else {
-        if (props.minWidth.xs)
-          widths += `@media (${breakpoints.xs}){
-              width: ${props.minWidth.xs}
-             }`;
-        if (props.minWidth.sm)
-          widths += `@media (${breakpoints.sm}){
-            width: ${props.minWidth.sm}
+        if (props.minWidth.lg)
+          widths += `@media (${breakpoints.lg}){
+            width: ${props.minWidth.lg}
              }`;
         if (props.minWidth.md)
           widths += `@media (${breakpoints.md}){
             width: ${props.minWidth.md}
              }`;
-        if (props.minWidth.lg)
-          widths += `@media (${breakpoints.lg}){
-            width: ${props.minWidth.lg}
+        if (props.minWidth.sm)
+          widths += `@media (${breakpoints.sm}){
+            width: ${props.minWidth.sm}
+             }`;
+        if (props.minWidth.xs)
+          widths += `@media (${breakpoints.xs}){
+              width: ${props.minWidth.xs}
              }`;
       }
     }

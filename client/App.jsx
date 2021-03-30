b6ac6365c853ce429fdd12472b8ffd57480a6f49
Christopher <calb200@aucklanduni.ac.nz>
2021-03-31 07:38:26 +1300

    setup shop page connection

--- client/App.jsx
+++ client/App.jsx
@@ -3,11 +3,13 @@ import "./app.css";
 import Navbar from "./components/navbar";
 import { Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Shop from "./pages/Shop"
 const App = () => {
   return (
     <Fragment>
       <Navbar />
       <Route path="/" exact component={Home} />
+      <Route path="/shop" exact component={Shop} />
     </Fragment>
   );
 };

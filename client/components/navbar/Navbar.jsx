4cbde08aefb39b574460d883b2ac88adf8719b88
Christopher <calb200@aucklanduni.ac.nz>
2021-04-03 10:43:13 +1300

    add buttons for auth to navbar

--- client/components/navbar/Navbar.jsx
+++ client/components/navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import JCUXContainer from "../jcux/JCUXContainer";
 import JCUXFlex from "../jcux/JCUXFlex";
 import styled from "styled-components";
 import JCUXText from "../jcux/JCUXText";
+import JCUXButton from "../jcux/JCUXButton";
 
 const StyledLink = styled(JCUXCustomLink)`
   text-decoration: none;
@@ -25,37 +26,43 @@ const Navbar = () => {
       height="fit-content"
       zIndex="1000"
     >
-      <JCUXContainer height="50px">
-        <JCUXFlex alignItems="center" height="100%" childSpacingRight="50px">
-          <StyledLink color="white" href="/">
-            <JCUXText
-              fontWeight="600"
-              type={{
-                xs: "sub-heading-small",
-                sm: "sub-heading-default",
-                md: "sub-heading-large",
-              }}
-              letterSpacing="5px"
-              marginTop="0px"
-              marginBottom="0px"
-            >
-              CHRISHTY
-            </JCUXText>
-          </StyledLink>
-          <StyledLink color="white" href="/shop">
-            <JCUXText
-              type={{
-                xs: "body-small",
-                sm: "body-default",
-                md: "body-large",
-              }}
-              letterSpacing="5px"
-              marginTop="0px"
-              marginBottom="0px"
-            >
-              SHOP
-            </JCUXText>
-          </StyledLink>
+      <JCUXContainer height="50px" width="100%">
+        <JCUXFlex justifyContent="space-between" width="100%" height="100%">
+          <JCUXFlex alignItems="center" childSpacingRight="50px">
+            <StyledLink color="white" href="/">
+              <JCUXText
+                fontWeight="600"
+                type={{
+                  xs: "sub-heading-small",
+                  sm: "sub-heading-default",
+                  md: "sub-heading-large",
+                }}
+                letterSpacing="5px"
+                marginTop="0px"
+                marginBottom="0px"
+              >
+                CHRISHTY
+              </JCUXText>
+            </StyledLink>
+            <StyledLink color="white" href="/shop">
+              <JCUXText
+                type={{
+                  xs: "body-small",
+                  sm: "body-default",
+                  md: "body-large",
+                }}
+                letterSpacing="5px"
+                marginTop="0px"
+                marginBottom="0px"
+              >
+                SHOP
+              </JCUXText>
+            </StyledLink>
+          </JCUXFlex>
+          <JCUXFlex childSpacingRight="10px">
+            <JCUXButton type="light-outlined">LOGIN</JCUXButton>
+            <JCUXButton type="tertiary">SIGN UP</JCUXButton>
+          </JCUXFlex>
         </JCUXFlex>
       </JCUXContainer>
     </JCUXFlex>
